49d6002161e55b4824034b0185fc946d3592cd6f
mcglaser <michaelcharlesglaser@gmail.com>
2016-08-15 13:52:22 -0700

    Removed community name from sign-up process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user	
-      redirect_to @user
+      redirect_to communities_path
     else
     render 'new'
      end
