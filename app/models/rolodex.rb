dd9d15e5ea637acd048cfbb9f491917a97dc5e9f
mcglaser <michaelcharlesglaser@gmail.com>
2017-02-09 13:49:26 -0800

    added

--- app/models/rolodex.rb
+++ app/models/rolodex.rb
@@ -4,4 +4,12 @@ class Rolodex < ActiveRecord::Base
   accepts_nested_attributes_for :referrals
   validates :nickname, presence: true
 
+
+  def referrals_attributes=(referral_attributes)
+    referral_attributes.values.each do |referral_attribute|
+      referral = Referral.find_or_create_by(referral_attribute)
+      self.referrals << referral
+    end
+  end
+
 end
